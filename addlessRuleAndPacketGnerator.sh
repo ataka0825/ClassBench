d909307998e51e420ace5dd4232bbc9cb5f4471f
Yuki Ishikawa <7023.yu.ki@gmail.com>
2016-04-15 17:44:49 +0900

    newscript

--- addlessRuleAndPacketGnerator.sh
+++ addlessRuleAndPacketGnerator.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 ./db_generator -bc acl1_seed 10000 2 0.5 -0.1 MyFilters10k # generate MyFilters10k
+cat MyFilters10k | awk {' print $1 '} | sed -e 's/@//g' > tmpRuleSACIDR
+cat MyFilters10k | awk {' print $2 '} > tmpRuleDACIDR
+./ZeroOneMask tmpRuleSACIDR > tmpRuleSAZOM
+./ZeroOneMask tmpRuleDACIDR > tmpRuleDAZOM
+paste -d '' tmpRuleSAZOM tmpRuleDAZOM > RuleZOM
 ./trace_generator 1 0.1 10 MyFilters10k # generate MyFilters10k_trace
 cat MyFilters10k_trace | awk {' print $1 '} > tmpPacketSA10
 cat MyFilters10k_trace | awk {' print $2 '} > tmpPacketDA10
