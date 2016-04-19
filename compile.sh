5a75700a2ef2a529c215f5fcf4340ab257d97eb2
Yuki Ishikawa <7023.yu.ki@gmail.com>
2016-04-19 18:57:55 +0900

    finishcompilescript

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-git clone git@github.com:tanakalab/ClassBench.git
-cd ClassBench
 wget http://www.arl.wustl.edu/classbench/db_generator.tar.gz
 wget http://www.arl.wustl.edu/classbench/trace_generator.tar.gz
 wget http://www.arl.wustl.edu/classbench/parameter_files.tar.gz
@@ -12,8 +10,9 @@ cd db_generator
 make all
 cd ../trace_generator
 make all
-cd ..
+cd ../
 cp db_generator/db_generator ./db_generator1
 cp trace_generator/trace_generator ./trace_generator1
 gcc -O3 CIDRNotationToZeroOneNotation.c -o ZeroOneMask -lm
 gcc -O3 tenTotwo.c -o tenTotwo -lm
+rm *.tar.gz
